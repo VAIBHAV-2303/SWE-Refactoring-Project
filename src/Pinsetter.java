efce28f7dc7432e11f231f0142dbc0eb58f27cf7
Surekha_AI <47828289+surekhamedapati@users.noreply.github.com>
2021-02-11 13:24:28 +0530

    Update Pinsetter.java

--- src/Pinsetter.java
+++ src/Pinsetter.java
@@ -149,7 +149,7 @@ public class Pinsetter {
 
 		try {
 			Thread.sleep(500);				// pinsetter is where delay will be in a real game
-		} catch (Exception e) {}
+		} catch (Exception e) { e.printStackTrace();}
 
 		sendEvent(count);
 
@@ -170,7 +170,7 @@ public class Pinsetter {
 		
 		try {
 			Thread.sleep(1000);
-		} catch (Exception e) {}
+		} catch (Exception e) { e.printStackTrace(); }
 		
 		sendEvent(-1);
 	}
